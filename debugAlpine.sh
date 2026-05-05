e24e60ba16551538f9016f5021464024f6a2ef38
Marcell Neszt <sylwolveryn@gmail.com>
2026-05-05 11:01:23 +0200

    update debug alpine with result of playwright resolve

--- debugAlpine.sh
+++ debugAlpine.sh
@@ -7,4 +7,4 @@ echo $PNPM_HOME
 echo $PATH
 ls -la $PNPM_HOME
 pnpm list -g
-node -e "console.log(require.resolve('playwright'))"
+node -e "console.log(require.resolve('playwright'))" #  /home/pwuser/app/node_modules/.pnpm/playwright@1.59.1/node_modules/playwright/index.js
